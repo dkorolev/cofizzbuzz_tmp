72e5336330952b87b7aec71c1ce813a464a9a549
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-24 22:39:01 +0200

    83 is now step09.cc

--- step83.cc
+++ step83.cc
@@ -1,4 +1,4 @@
-// Logging thread names, to illustrate the "work" is done by a large number of "dedicated" threads.
+// Explicitly verbose, ugly, event-driven logic. Work still initiated and executed by dedicated threads.
 
 #include <iostream>
 #include <string>
@@ -9,6 +9,8 @@
 #include <future>
 #include <thread>
 #include <atomic>
+#include <mutex>
+#include <memory>
 
 using std::cout;
 using std::endl;
@@ -17,9 +19,14 @@ using std::queue;
 using std::string;
 using std::to_string;
 using namespace std::chrono_literals;
+using std::atomic_bool;
 using std::atomic_int;
 using std::future;
+using std::lock_guard;
+using std::make_shared;
+using std::mutex;
 using std::promise;
+using std::shared_ptr;
 using std::thread;
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
@@ -67,32 +74,55 @@ inline void IsDivisibleByFive(int value, function<void(bool)> cb) {
 struct FizzBuzzGenerator {
   int value = 0;
   queue<string> next_values;
+  void InvokeCbThenNext(function<void(string)> cb, function<void()> next) {
+    cb(next_values.front());
+    next_values.pop();
+    next();
+  }
   void Next(function<void(string)> cb, function<void()> next) {
-    auto const InvokeCbThenNext = [this, cb, next]() {
-      cb(next_values.front());
-      next_values.pop();
-      next();
+    struct AsyncCaller {
+      FizzBuzzGenerator* self;
+      function<void(string)> cb;
+      function<void()> next;
+      AsyncCaller(FizzBuzzGenerator* self, function<void(string)> cb, function<void()> next)
+          : self(self), cb(std::move(cb)), next(std::move(next)) {}
+      mutex mut;
+      bool has_d3 = false;
+      bool has_d5 = false;
+      bool d3;
+      bool d5;
+      void ActIfHasAllInputs() {
+        if (has_d3 && has_d5) {
+          if (d3) {
+            self->next_values.push("Fizz");
+          }
+          if (d5) {
+            self->next_values.push("Buzz");
+          }
+          if (!d3 && !d5) {
+            self->next_values.push(to_string(self->value));
+          }
+          self->InvokeCbThenNext(std::move(cb), std::move(next));
+        }
+      }
     };
-    if (next_values.empty()) {
-      ++value;
-      promise<bool> pd3;
-      promise<bool> pd5;
-      IsDivisibleByThree(value, [&pd3](bool d3) { pd3.set_value(d3); });
-      IsDivisibleByFive(value, [&pd5](bool d5) { pd5.set_value(d5); });
-      bool const d3 = pd3.get_future().get();
-      bool const d5 = pd5.get_future().get();
-      if (d3) {
-        next_values.push("Fizz");
-      }
-      if (d5) {
-        next_values.push("Buzz");
-      }
-      if (!d3 && !d5) {
-        next_values.push(to_string(value));
-      }
-      InvokeCbThenNext();
+    if (!next_values.empty()) {
+      InvokeCbThenNext(cb, next);
     } else {
-      InvokeCbThenNext();
+      ++value;
+      auto shared_async_caller_instance = make_shared<AsyncCaller>(this, std::move(cb), std::move(next));
+      IsDivisibleByThree(value, [shared_async_caller_instance](bool d3) {
+        lock_guard<mutex> lock(shared_async_caller_instance->mut);
+        shared_async_caller_instance->d3 = d3;
+        shared_async_caller_instance->has_d3 = true;
+        shared_async_caller_instance->ActIfHasAllInputs();
+      });
+      IsDivisibleByFive(value, [shared_async_caller_instance](bool d5) {
+        lock_guard<mutex> lock(shared_async_caller_instance->mut);
+        shared_async_caller_instance->d5 = d5;
+        shared_async_caller_instance->has_d5 = true;
+        shared_async_caller_instance->ActIfHasAllInputs();
+      });
     }
   }
 };
@@ -112,6 +142,10 @@ int main() {
   int total = 0;
   auto t0 = TimestampMS();
 
+  // A quick & hacky way to wait until everything is done.
+  mutex unlocked_when_done;
+  unlocked_when_done.lock();
+
   function<void(string)> Print = [&total, &t0](string s) {
     auto t1 = TimestampMS();
     cout << ++total << " : " << s << ", in " << (t1 - t0) << "ms, from thread " << CurrentThreadName() << endl;
@@ -121,10 +155,15 @@ int main() {
   function<void()> KeepGoing = [&]() {
     if (total < 15) {
       g.Next(Print, KeepGoing);
+    } else {
+      unlocked_when_done.unlock();
     }
   };
 
   // Kick off the run.
   // It will initiate the series of "call back-s", via the executor, from its thread.
   KeepGoing();
+
+  // NOTE(dkorolev): Now we must wait, otherwise the destroyed instance of `g` will be used from other threads.
+  unlocked_when_done.lock();
 }
