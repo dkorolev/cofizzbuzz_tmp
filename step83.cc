f70c2237b1ceba6febe083cdf05a35c77cfd4b66
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-24 22:32:48 +0200

    83 is now step01.cc

--- step83.cc
+++ step83.cc
@@ -1,9 +1,32 @@
-// Test setup, dummy code so far.
+// Initial FizzBuzz. It skips 15 and prints 16 lines, since sometimes one `.Next()` yields more than one callback call.
 
 #include <iostream>
+#include <string>
+#include <functional>
 
 using std::cout;
 using std::endl;
+using std::function;
+using std::string;
+using std::to_string;
+
+struct FizzBuzzGenerator {
+  int value = 0;
+  void Next(function<void(string)> cb) {
+    ++value;
+    bool const d3 = (value % 3 == 0);
+    bool const d5 = (value % 5 == 0);
+    if (d3) {
+      cb("Fizz");
+    }
+    if (d5) {
+      cb("Buzz");
+    }
+    if (!d3 && !d5) {
+      cb(to_string(value));
+    }
+  }
+};
 
 int main() {
 #if defined(NDEBUG) && !defined(DEBUG)
@@ -14,5 +37,9 @@ int main() {
 #error "Must have either `DEBUG` or `NDEBUG` `#define`-d."
 #endif
 
-  cout << "Hello, Coroutines!" << endl;
+  FizzBuzzGenerator g;
+  int total = 0;
+  while (total < 15) {
+    g.Next([&total](string s) { cout << ++total << " : " << s << endl; });
+  }
 }
