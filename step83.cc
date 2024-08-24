0233b5be3c013b294ca2b4a64ddc2b2c602c4a3c
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-24 22:33:34 +0200

    83 is now step02.cc

--- step83.cc
+++ step83.cc
@@ -1,30 +1,37 @@
-// Initial FizzBuzz. It skips 15 and prints 16 lines, since sometimes one `.Next()` yields more than one callback call.
+// Introduced the "cache for the yet unseen value". Stops at 15 now.
 
 #include <iostream>
 #include <string>
 #include <functional>
+#include <queue>
 
 using std::cout;
 using std::endl;
 using std::function;
+using std::queue;
 using std::string;
 using std::to_string;
 
 struct FizzBuzzGenerator {
   int value = 0;
+  queue<string> next_values;
   void Next(function<void(string)> cb) {
-    ++value;
-    bool const d3 = (value % 3 == 0);
-    bool const d5 = (value % 5 == 0);
-    if (d3) {
-      cb("Fizz");
+    if (next_values.empty()) {
+      ++value;
+      bool const d3 = (value % 3 == 0);
+      bool const d5 = (value % 5 == 0);
+      if (d3) {
+        next_values.push("Fizz");
+      }
+      if (d5) {
+        next_values.push("Buzz");
+      }
+      if (!d3 && !d5) {
+        next_values.push(to_string(value));
+      }
     }
-    if (d5) {
-      cb("Buzz");
-    }
-    if (!d3 && !d5) {
-      cb(to_string(value));
-    }
+    cb(next_values.front());
+    next_values.pop();
   }
 };
 
